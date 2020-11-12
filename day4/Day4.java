e1a0c5a6fefcec1ad6f550d7b63520e4324381bd
rhtld123 <rhtld123@gmail.com>
2020-11-13 01:24:41 +0900

    Day 4 퀴즈완료

--- day4/Day4.java
+++ day4/Day4.java
@@ -1,30 +1,39 @@
 public class Day4{
-	public static int[] nbMonths(float startPriceOld, float startPriceNew, float savingperMonth,float percentLossByMonth) {
+	public static int[] nbMonths(double startPriceOld, double startPriceNew, double savingperMonth,double percentLossByMonth) {
 		int resultArray[] = null; // 리턴 Array
-		int month = 1;
+		int month = 0;
 		int money = 0;
-		float result=0; //계산할 돈
-		float percentLossByMonthTmp=(float) (percentLossByMonth * 0.01); //달에 내려가는 가격
-		float startPriceOldTmp = (float)(startPriceOld - (startPriceOld * percentLossByMonthTmp)); // 예전 차 비용(감가 포함)
-		float startPriceNewTmp = (float)(startPriceNew - (startPriceNew * percentLossByMonthTmp)); // 새 차 비용(감가 포함)
-		while (true) {
-			if (month % 2 == 0)  {// 2개월 마다 체크
-				percentLossByMonthTmp += 0.005;
+		double result=0; //계산할 돈
+		double percentLossByMonthTmp= percentLossByMonth * 0.01; //달에 내려가는 가격
+		result = startPriceOld - startPriceNew; // 예전 차 - 새차  
+		if (result > 0) {
+			money = (int)result;
+			resultArray = new int[]{ month , money };
+			return resultArray;
+		}else {
+			month++;
+			double startPriceOldTmp = startPriceOld - (startPriceOld * percentLossByMonthTmp); // 예전 차 비용(감가 포함)
+			double startPriceNewTmp = startPriceNew - (startPriceNew * percentLossByMonthTmp); // 새 차 비용(감가 포함)
+			while (true) {
+				if (month % 2 == 0)  {// 2개월 마다 체크.
+					percentLossByMonthTmp += 0.005;
+				}
+				if (month>1) {
+					startPriceOldTmp = startPriceOldTmp - (startPriceOldTmp * percentLossByMonthTmp);
+					startPriceNewTmp = startPriceNewTmp - (startPriceNewTmp * percentLossByMonthTmp);				
+				}
+				result = ((startPriceOldTmp + savingperMonth * month) - startPriceNewTmp); // 예전 차 + (저축금액 * 달 ) - 새차 
+				if (result > 0) {
+					money = (int)result;
+					break;
+				}
+				else {
+					month++;	
+				}
 			}
-			if (month>1) {
-				startPriceOldTmp = (float)(startPriceOldTmp - (startPriceOldTmp * percentLossByMonthTmp));
-				startPriceNewTmp = (float)(startPriceNewTmp - (startPriceNewTmp * percentLossByMonthTmp));				
-			}
-			result = (float)((startPriceOldTmp + savingperMonth * month) - startPriceNewTmp); // 예전 차 + (저축금액 * 달 ) - 새차 
-			if (result > 0) {
-				money = (int) result;
-				break;
-			}
-			else {
-				month++;	
-			}
+			resultArray = new int[]{ month , money };
+			return resultArray;
+			
 		}
-		resultArray = new int[]{ month , money };
-		return resultArray;
 	}
 }
--- day4/Day4Test.java
+++ day4/Day4Test.java
@@ -3,9 +3,17 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-public class Day4Test {
+public class Day4Test { /* Day 4 Test */
 	@Test
 	public void testCase1() {
+		int[] result= new int[2];
+		result = Day4.nbMonths(12000, 8000, 1000, (float)1.5);
+		
+		assertArrayEquals(result,new int[] {0,4000});
+	}
+	
+	@Test
+	public void testCase2() {
 		int[] result= new int[2];
 		result = Day4.nbMonths(2000, 8000, 1000, (float)1.5);
 		assertArrayEquals(result,new int[] {6,766});
