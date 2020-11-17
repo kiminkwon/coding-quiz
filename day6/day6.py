0ebdf595bc47ab20f805421b91245d16eede2fb3
phisrood <kym1316@naver.com>
2020-11-17 21:42:17 +0900

    Create day6.py
    Day6 퀴즈완료

---
+++ day6/day6.py
@@ -0,0 +1,22 @@
+"""
+한 번만 나타나는 요소 (x라고합시다)와 두 번만 나타나는 요소 (y라고합시다)를 제외하고 모든 요소가 세 번 나타나는 양의 정수 배열이 제공됩니다.
+당신의 임무는 x * x * y를 찾는 것입니다.
+Example
+For arr=[1, 1, 1, 2, 2, 3], the result should be 18
+3 x 3 x 2 = 18
+For arr=[6, 5, 4, 100, 6, 5, 4, 100, 6, 5, 4, 200], the result should be 4000000
+200 x 200 x 100 = 4000000
+
+missingValues(arr) { }
+"""
+from functools import reduce
+
+def missinValues(arr):
+
+    return(reduce(lambda x,y: x*y, set(map(lambda x: x*x if arr.count(x)==1 else x if arr.count(x)==2 else 1 , arr))))
+
+
+a= [1, 1, 1, 2, 2, 3]
+b=[6, 5, 4, 100, 6, 5, 4, 100, 6, 5, 4, 200]
+print(missinValues(a))
+print(missinValues(b))
